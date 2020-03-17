0a1b124e0b09b168e7f35edc59e0a3341663a8bc
lalogaribo <garibo.eduardo90@gmail.com>
2020-03-17 13:40:25 -0700

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,33 @@
 class Customer
-  
+  attr_accessor :name, :age
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    save
+  end
+
+  def new_meal(waiter, total, tip)
+    meal = Meal.new(waiter, self, total, tip)
+    meal
+  end
+
+  def meals
+    Meal.all.select { |meal| meal if meal.customer == self }
+  end
+
+  def waiters
+    Meal.all.collect { |meal| meal.waiter if meal.customer == self }.compact!
+  end
+
+  private
+
+  def save
+    @@all << self
+  end
 end
--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,22 @@
 class Meal
+  attr_accessor :waiter, :customer, :total, :tip
+  @@all = []
 
+  def self.all
+    @@all
+  end
+
+  def initialize(waiter, customer, total, tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    save
+  end
+
+  private
+
+  def save
+    @@all << self
+  end
 end
